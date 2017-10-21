71482fab5228a56604c108d93af4bffff0fcc33c
Christopher Thomas <ChristopherCThomas94@gmail.com>
2017-10-21 11:12:28 -0500

    Add Test Option

--- __main__.py
+++ __main__.py
@@ -8,5 +8,12 @@ if sys.version_info < (3, 0):
     print("Python version 3+ required")
     print("Download Python 3: https://www.python.org/downloads/")
 
-my_window = Window()
-my_window.mainloop()
+# Main Code
+test = True
+
+if test:
+    from test import console
+    console.start()
+else:
+    my_window = Window()
+    my_window.mainloop()
